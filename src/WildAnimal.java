c67ae96957f32a124167ecea599d9f65a43524a2
ali <aliriyahi.sut@gmail.com>
2021-06-19 23:17:37 +0430

    7

--- src/WildAnimal.java
+++ src/WildAnimal.java
@@ -1,36 +1,46 @@
+import java.util.Random;
+
 public class WildAnimal {
+    Random rand = new Random();
 
     public String animalname;
     public int speed;
-    public int xposision = (int)Math.random()%6;
-    public int yposision = (int)Math.random()%6;
+    public int xposision = Math.abs(rand.nextInt()%6);
+    public int yposision = Math.abs(rand.nextInt()%6);
     public int cages = 0;
     public int maxCage;
+    public int removeTime = 0;
 
     public void animalmove(){
-        int random= (int) (Math.random()%2);
+        int random = Math.abs(rand.nextInt()%2);
         if (random==0)
             random=1;
         else if (random==1)
             random=-1;
         int move=random*this.speed;
-        int random1=(int)Math.random()%2;
+        int random1 = Math.abs(rand.nextInt()%2);
 
         if (random1==0){
             if (this.xposision+move<=5 && this.xposision+move>=0){
                 this.xposision+=move;
+                //System.out.println("??????????????????????????????????????????");
+            }else {
+                move *= -1;
+                this.xposision += move;
+                //System.out.println(move);
             }
-            move*=-1;
-            this.xposision+=move;
 
         }
 
         else if (random1==1){
             if (this.yposision+move<=5 && this.yposision+move>=0){
                 this.yposision+=move;
+                //System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            }else {
+                move *= -1;
+                this.yposision += move;
+                //System.out.println(move);
             }
-            move*=-1;
-            this.yposision+=move;
 
         }
     }
